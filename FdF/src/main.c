cc1a5c0880d1caadfe77d236c475fead838ce59c
Genís Picó Cordo <genis.pico@hotmail.com>
2024-12-23 16:38:05 +0100

    Update main.c

--- FdF/src/main.c
+++ FdF/src/main.c
@@ -11,24 +11,35 @@
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
-#include "MLX42/MLX42.h"
 
-void handle_key(mlx_key_data_t keydata, void* param) 
+void	handle_key(mlx_key_data_t keydata, void *param)
 {
-    if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
-        mlx_close_window((mlx_t*)param);
+	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
+		mlx_close_window((mlx_t *)param);
 }
 
 int	main(void)
 {
-	mlx_t*	mlx = mlx_init(800, 600, "Mi primera ventana con MLX42", true);
+	mlx_t		*mlx;
+	mlx_image_t	*img;
+
+	mlx = mlx_init(800, 600, "Dibujo un píxel", true);
 	if (!mlx)
 	{
 		ft_putstr_fd("Error al inicializar MLX42\n", 2);
-		return EXIT_FAILURE;
+		return (EXIT_FAILURE);
 	}
+	img = mlx_new_image(mlx, 800, 600);
+	if (!img)
+	{
+		ft_putstr_fd("Error al crear imagen\n", 2);
+		mlx_terminate(mlx);
+		return (EXIT_FAILURE);
+	}
+	mlx_put_pixel(img, 400, 300, 0xFFFFFFFF);
+	mlx_image_to_window(mlx, img, 0, 0);
 	mlx_key_hook(mlx, handle_key, mlx);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
-	return EXIT_SUCCESS;
+	return (EXIT_SUCCESS);
 }
