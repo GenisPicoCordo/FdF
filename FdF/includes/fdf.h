0566e2ee1582022fe26654fa5c9bcd5746e24978
Genís Picó Cordo <genis.pico@hotmail.com>
2025-01-14 17:55:18 +0100

    Update fdf.h

--- FdF/includes/fdf.h
+++ FdF/includes/fdf.h
@@ -25,7 +25,7 @@
 # include "../libft/libft.h"
 # include "../MLX42/include/MLX42/MLX42.h"
 
-// Estructura para las coordenadas y el color de una línea
+// Estructuras
 typedef struct s_line
 {
 	int			x0;
@@ -35,7 +35,6 @@ typedef struct s_line
 	uint32_t	color;
 }	t_line;
 
-// Estructura para las variables de cálculo
 typedef struct s_vars
 {
 	int	dx;
@@ -45,7 +44,6 @@ typedef struct s_vars
 	int	err;
 }	t_vars;
 
-// Estructura para guardar datos del mapa
 typedef struct s_map
 {
 	int	**z_values;
@@ -53,18 +51,51 @@ typedef struct s_map
 	int	height;
 }	t_map;
 
+// Nueva estructura para espaciado y offsets
+typedef struct s_params
+{
+	int	spacing;
+	int	offset_x;
+	int	offset_y;
+}	t_params;
+
+// Estructura para agrupar los parámetros comunes de las líneas
+typedef struct s_draw_params
+{
+	t_map		*map;
+	t_params	*params;
+}	t_draw_params;
+
+// Estructura auxiliar para manejar los cálculos de Bresenham
+typedef struct s_bresenham
+{
+	int	dx;
+	int	dy;
+	int	sx;
+	int	sy;
+	int	err;
+}	t_bresenham;
+
 // Prototipos de funciones
-void	handle_key(mlx_key_data_t keydata, void *param);
-void	draw_line(mlx_image_t *img, t_line *line);
-void	init_vars(t_vars *vars, t_line *line);
+void	draw_line_basic(mlx_image_t *img, t_line *line, uint32_t color);
 void	isometric_projection(int *x, int *y, int z);
 t_map	*read_fdf(const char *filename);
-
-// Prototipos de las funciones adicionales
+void	free_map(t_map *map);
 int		*process_line(char *line, int *width);
 int		**resize_array(int **array, int old_size, int new_size);
-int		init_graphics(mlx_t **mlx, mlx_image_t **img);
-void	free_map(t_map *map);
-void	draw_map_lines(mlx_image_t *img, t_map *map);
+void	draw_line(mlx_image_t *img, t_line *line);
+void	draw_grid(mlx_image_t *img, t_map *map);
+void	init_bresenham(t_bresenham *b, t_line *line);
+int		is_pixel_in_bounds(int x, int y);
+void	handle_key(mlx_key_data_t keydata, void *param);
+int		init_mlx(mlx_t **mlx, mlx_image_t **img);
+t_map	*load_map(const char *filename, mlx_t *mlx);
+void	calculate_spacing_and_offsets(t_map *map, t_params *params);
+void	draw_grid_line(mlx_image_t *img, t_line *line, \
+	t_map *map, t_params *params);
+void	draw_grid_horizontal(mlx_image_t *img, \
+	t_draw_params *draw_params, int x, int y);
+void	draw_grid_vertical(mlx_image_t *img, \
+	t_draw_params *draw_params, int x, int y);
 
 #endif
