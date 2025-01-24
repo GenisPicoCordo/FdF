f1aa10fc199652208ad639cfab7a9564bb8e3b1b
Genís Picó Cordo <genis.pico@hotmail.com>
2025-01-24 11:30:43 +0100

    Update fdf.h

--- FdF/includes/fdf.h
+++ FdF/includes/fdf.h
@@ -13,114 +13,130 @@
 #ifndef FDF_H
 # define FDF_H
 
-# define WINDOW_WIDTH 1920
-# define WINDOW_HEIGHT 1080
-
-# include "MLX42/MLX42.h"
-# include "../libft/libft.h"
-# include <math.h>
 # include <stdlib.h>
-# include <stdint.h>
+# include <unistd.h>
+# include <math.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdbool.h>
+# include "MLX42/MLX42.h"
+# include "libft.h"
 
-//	Gradiente de colores
-# define COLOUR_ONE     0xFF6F61ff   // Coral (tono cálido)
-# define COLOUR_TWO     0xFFA177ff   // Melón (tono intermedio)
-# define COLOUR_THREE   0xFFD166ff   // Amarillo claro (tono vibrante)
-# define COLOUR_FOUR    0x6B5B95ff   // Lavanda (tono frío)
-# define COLOUR_FIVE    0x88B04Bff   // Verde pera (tono natural)
-# define COLOUR_SIX     0x92A8D1ff   // Azul pastel (tono suave)
-# define COLOUR_SEVEN   0x955251ff   // Rojo terracota (tono cálido)
-# define COLOUR_EIGHT   0xB565A7ff   // Rosa púrpura (tono vibrante)
-# define COLOUR_NINE    0x009B77ff   // Verde esmeralda (tono fresco)
-# define COLOUR_TEN     0xDD4124ff   // Rojo intenso (tono llamativo)
-# define BACKGROUND_COLOR  0x2C2C2CFF  // Gris oscuro (fondo neutro)
+# define WINDOW_WIDTH 1280
+# define WINDOW_HEIGHT 720
 
-// Estructuras
-typedef struct s_line
+# define COLOUR_ONE 0xB8890Fff
+# define COLOUR_TWO 0x80960dff
+# define COLOUR_THREE 0x4b902dff
+# define COLOUR_FOUR 0x008846ff
+# define COLOUR_FIVE 0x00765dff
+# define COLOUR_SIX 0x006563ff
+# define COLOUR_SEVEN 0x005563ff
+# define COLOUR_EIGHT 0x004463ff
+# define COLOUR_NINE 0x003266ff
+# define COLOUR_TEN 0x02007cff
+
+# define PI 3.14159265359
+
+typedef struct s_point
 {
-	int			x0;
-	int			y0;
-	int			x1;
-	int			y1;
-	uint32_t	color;
-}	t_line;
+	int	x;
+	int	y;
+	int	z;
+	int	colour;
+}	t_point;
+
+typedef struct s_fpoint
+{
+	float	x;
+	float	y;
+	int		colour;
+}	t_fpoint;
+
+typedef struct s_delta
+{
+	float	dx;
+	float	dy;
+	float	x;
+	float	y;
+	int		step_count;
+}	t_delta;
 
 typedef struct s_map
 {
-	int	**z_values;
-	int	width;
-	int	height;
-	int	min_z;
-	int	max_z;
-	int	scale;
+	int			rows;
+	int			cols;
+	int			max;
+	int			min;
+	float		scaling_factor;
+	float		vertical_scale;
+	float		projection_angle_alpha;
+	float		projection_angle_beta;
+	float		zoom_level;
+	int			flat_projection_mode;
+	int			default_color;
+	int			width_offset;
+	int			height_offset;
+	char		**grid2d;
+	t_point		**grid3d;
+	t_fpoint	**fgrid;
 }	t_map;
 
-typedef struct s_step
+typedef struct s_fdf
 {
-	float	x_step;
-	float	y_step;
-	float	step;
-}	t_step;
-
-typedef struct s_draw_info
-{
-	mlx_image_t	*img;
-	int			x;
-	int			y;
-	int			z0;
-	int			z1;
-	int			offset_x;
-	int			offset_y;
-	int			window_width;
-	int			window_height;
-	int			scale;
 	t_map		*map;
-}	t_draw_info;
+	mlx_t		*mlx;
+	mlx_image_t	*img;
+}	t_fdf;
 
-// Prototipos de funciones main_utils
-void		handle_key(mlx_key_data_t keydata, void *param);
-int			init_mlx(mlx_t **mlx, mlx_image_t **img);
+// Function declarations
+void	initialize_map(t_map *map);
+void	release_memory(t_map *map);
+void	free_allocated_array(void **array);
+void	print_error_message(int error_code);
 
-// Prototipos de funciones mapa
-int			get_map_dimensions(const char *filename, int *width, int *height);
-t_map		*init_map(int width, int height);
-void		free_map(t_map *map);
-int			read_lines_to_map(int fd, t_map *map, int width, int height);
-t_map		*read_fdf(const char *filename);
-t_map		*initialize_map(const char *filename, int *width, int *height);
+int		validate_file_extension(char *file);
+int		count_lines_in_file(int fd);
+void	read_file_data(int fd, t_map *map);
 
-// Prototipos de funciones map_utils
-int			count_lines_and_width(int fd, int *width, int *height);
-int			**allocate_z_values(int width, int height);
-int			ft_count_words(const char *str);
-int			process_line(char *line, t_map *map, int y, int width);
-void		scale_z_values(t_map *map);
+void	allocate_grid_memory(t_map *map);
+void	calculate_columns(t_map *map);
+void	populate_columns(t_map *map, int row);
+void	generate_matrix(t_map *map);
 
-// Prototipos de funciones draw
-void		set_line_points(t_line *line, t_draw_info *info, int is_horizontal);
-void		draw_grid_line(t_draw_info *info, \
-	int is_horizontal, uint32_t color);
-void		set_info_values(t_draw_info *info, mlx_image_t *img, int x, int y);
-void		draw_grid_segments(t_draw_info *info, \
-	t_map *map, int x, int y);
-void		draw_isometric_grid(mlx_image_t *img, t_map *map, \
-	int window_width, int window_height);
+void	prepare_projection_data(t_map *map);
+void	assign_color_to_point(t_map *map, int i, int j);
 
-// Prototipos de funciones draw_utils1
-void		apply_isometric_projection(int *x, int *y, int z);
-t_step		calculate_steps(t_line *line);
-void		draw_pixels(mlx_image_t *img, t_line *line, \
-	t_step *step, uint32_t color);
-void		draw_line_basic(mlx_image_t *img, t_line *line, uint32_t color);
-void		apply_transformations(t_line *line, t_draw_info *info);
+int		create_window_and_render(t_map *map);
+void	handle_key_events(void *param);
+void	handle_rotation_keys(void *param);
 
-// Prototipos de funciones draw_utils2
-int			is_pixel_in_bounds(int x, int y);
-uint32_t	get_color(int z, int min_z, int max_z);
+float	calculate_color_percentage(int max, int min, int current);
+int		get_color_based_on_percentage(double percentage);
+int		get_maximum(int a, int b);
+int		get_minimum(int a, int b);
 
-// Prototipos de funciones input_utils
-int			check_extension(const char *file);
-int			check_input(const char *file);
-void		print_error(const char *msg);
+void	calculate_projection(t_map *map, int i, int j);
+void	populate_columns(t_map *map, int row);
+void	set_2d_projection_mode(t_map *map);
+
+void	update_delta_values(t_delta *delta, t_fpoint start, t_fpoint end);
+void	draw_line_segment(t_fdf *fdf, t_fpoint start, t_fpoint end);
+void	connect_grid_points(t_fdf *fdf, int x, int y);
+
+void	render_grid(void *param);
+void	handle_all_hooks(void *param);
+
+// Libft-based utility functions
+int		is_valid_character(char c);
+char	**split_line(const char *str, char delimiter);
+int		convert_to_int(const char *str);
+int		contains_comma(const char *str);
+int		extract_color_from_line(const char *str);
+void	process_input_data(char **argv, t_map *map);
+void	reset_image(t_fdf *fdf);
+void	adjust_scaling_factor(t_map *map);
 
 #endif
